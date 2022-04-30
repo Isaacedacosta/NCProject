09d1b2e219d27c1a6fa1d1d00e5646c29df33146
Isaac <isaacdacosta@hotmail.com>
2022-04-30 17:48:32 -0300

    Created SQL project, published to create DB, Created DB Management Class

---
+++ NCProjectApplication/DbConnectionExtended.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCProjectApplication
+{
+    class DbConnectionExtended
+    {
+        public string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog = NCProject; Integrated Security = True";
+
+
+    }
+}
---
+++ NCProjectData/dbo/Tables/Locations.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Locations]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] NCHAR(40) NOT NULL, 
+    [Descricao] NCHAR(100) NOT NULL, 
+    [Localizacao] NCHAR(40) NOT NULL, 
+    [Cidade] NCHAR(30) NOT NULL, 
+    [Estado] CHAR(2) NOT NULL
+)
